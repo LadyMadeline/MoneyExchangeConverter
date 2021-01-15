a134c5ad70387467798531b386a9814908590179
Елена Ситникова <cheshiredog1865@gmail.com>
2021-01-15 13:14:38 +0200

    [added] - bootstrap

--- Startup.cs
+++ Startup.cs
@@ -25,28 +25,17 @@ namespace MoneyExchangeConverter
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddMvc();
-            //services.AddControllers();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoneyExchangeConverter", Version = "v1" });
-            //});
-
             services.AddDbContext<ExchangeContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("ExchangeContext")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
-            //app.UseSwagger();
-            //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoneyExchangeConverter v1"));
-
 
             app.UseHttpsRedirection();
 
